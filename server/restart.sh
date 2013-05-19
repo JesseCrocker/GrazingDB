883d71d8276f3b79fae37136d16645b246abf9d3
Jesse Crocker <jesse@DataMongers.net>
2013-05-18 23:23:50 -0700

    fix restart script

--- server/restart.sh
+++ server/restart.sh
@@ -4,4 +4,4 @@ PIDFILE=/home/deploy/django.pid
 kill -9 `cat $PIDFILE`
 
 sleep 10
-python /home/deploy/grazingDB/webapp/manage.py run_gunicorn --daemon -w 3 -p $PIDFILE
+python /home/deploy/GrazingDB/webapp/manage.py run_gunicorn --daemon -w 3 -p $PIDFILE
