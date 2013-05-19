fdb60e5b949844eaf0620f8b35c9119d790504b1
Jesse Crocker <jesse@DataMongers.net>
2013-05-19 10:25:42 -0700

    login/lgout links in base template, html table styleing

--- webapp/grazingDB/settings.py
+++ webapp/grazingDB/settings.py
@@ -108,6 +108,11 @@ WSGI_APPLICATION = 'grazingDB.wsgi.application'
 REST_FRAMEWORK = {
 }
 
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+    'django.core.context_processors.request',
+)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
