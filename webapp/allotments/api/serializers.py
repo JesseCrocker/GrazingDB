369ba24c8a19ab4a884fd9a7f1f99f8be8850334
Jesse Crocker <jesse@DataMongers.net>
2013-05-20 20:59:29 -0700

    fix serializer

--- webapp/allotments/api/serializers.py
+++ webapp/allotments/api/serializers.py
@@ -12,7 +12,6 @@ class GeoAllotmentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Allotment
-        read_only_fields = ('geometry',)
 
     def simplified_geometry(self, obj):
         return obj.simplified_geometry()
